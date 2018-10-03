e6828c91c0bd587be3472756e098c8cb87896f52
Adrian Buenache <arhacai@gmail.com>
2018-10-03 15:16:06 +0200

    Added files to repository

---
+++ story/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, get_object_or_404
+
+from .models import Story
+
+
+def homepage(request):
+    prologue = Story.objects.get(title='Prologo')
+    stories = Story.objects.exclude(title=prologue.title)
+    return render(
+        request, 'index.html', {'prologue': prologue, 'stories': stories}
+    )
+
+
+def story_detail(request, pk):
+    story = get_object_or_404(Story, pk=pk)
+    return render(request, 'story/story_detail.html', {'story': story})
